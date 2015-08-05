ae2768db82b38cb9f86e0e4656b40f06fdf6d3e9
Peng Wang <peng@pengxwang.com>
2015-08-05 09:53:19 -0700

    Map should current user location right away
    Achieve this by showing activity indicator until current location
    exists.
    
    Address the edge case of map remaining hidden on denial by still
    revealing, but with an alert.

--- MapViewController/MapViewController.swift
+++ MapViewController/MapViewController.swift
@@ -12,6 +12,7 @@ import UIKit
 
 class MapViewController: UIViewController {
 
+    @IBOutlet weak var mapLoadingIndicator: UIActivityIndicatorView!
     @IBOutlet weak var mapView: MKMapView!
 
     var locationManager: CLLocationManager!
@@ -35,10 +36,42 @@ class MapViewController: UIViewController {
             self.locationManager.requestAlwaysAuthorization()
         } else if status != .Denied {
             self.mapView.showsUserLocation = true
+        } else {
+            self.handleLocationAuthorizationDenial()
         }
     }
 
-    private func zoomToLocation(location: CLLocation) {
+    private func handleLocationAuthorizationDenial() {
+        let alertController = UIAlertController(
+            title: "Understood",
+            message: "You've denied sharing your current location. You can always find your location manually.",
+            preferredStyle: .Alert
+        )
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default) { (action) in
+            alertController.dismissViewControllerAnimated(true, completion: nil)
+        })
+        self.presentViewController(alertController, animated: true, completion: nil)
+        self.revealMapView()
+    }
+
+    private func revealMapView(completion: (() -> Void)? = nil) {
+        guard self.mapView.hidden else { return }
+
+        self.mapLoadingIndicator.stopAnimating()
+
+        self.mapView.alpha = 0.0
+        self.mapView.hidden = false
+        UIView.animateWithDuration(0.3) {
+            self.mapView.alpha = 1.0
+            if let completion = completion {
+                completion()
+            }
+        }
+    }
+
+    private func zoomToLocation(location: CLLocation, animated: Bool) {
+        self.revealMapView()
+
         let spanDegrees = 0.03
         let region = MKCoordinateRegion(
             center: CLLocationCoordinate2D(
@@ -48,7 +81,7 @@ class MapViewController: UIViewController {
             span: MKCoordinateSpanMake(spanDegrees, spanDegrees)
         )
 
-        self.mapView.setRegion(region, animated: true)
+        self.mapView.setRegion(region, animated: animated)
     }
 
     // MARK: Actions
@@ -66,6 +99,8 @@ extension MapViewController: CLLocationManagerDelegate {
     func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
         if status == .AuthorizedWhenInUse || status == .AuthorizedAlways {
             self.mapView.showsUserLocation = true
+        } else {
+            self.handleLocationAuthorizationDenial()
         }
     }
 
@@ -77,7 +112,7 @@ extension MapViewController: MKMapViewDelegate {
 
     func mapView(mapView: MKMapView, didUpdateUserLocation userLocation: MKUserLocation) {
         guard let location = userLocation.location else { return }
-        self.zoomToLocation(location)
+        self.zoomToLocation(location, animated: false)
     }
 
 }
