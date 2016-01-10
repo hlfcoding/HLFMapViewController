e47d906c6378a865a402d8e5c92d3f97724b61aa
Peng Wang <peng@pengxwang.com>
2016-01-09 20:46:39 -0800

    Map modal in example app is missing a back button
    Instead simplify with programmatic push/pop.

--- Example/HLFMapViewController/ViewController.swift
+++ Example/HLFMapViewController/ViewController.swift
@@ -13,19 +13,18 @@ import HLFMapViewController
 
 class ViewController: UIViewController, MapViewControllerDelegate {
 
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        guard segue.identifier == "presentMap",
-              let navigationController = segue.destinationViewController as? UINavigationController
-              else { return }
+    @IBAction func showMap(sender: AnyObject) {
         let mapViewController = MapViewController(nibName: "MapViewController", bundle: MapViewController.bundle)
         mapViewController.title = NSLocalizedString("Select Nearby Location", comment: "")
         mapViewController.delegate = self
-        navigationController.pushViewController(mapViewController, animated: false)
+
+        self.navigationController?.pushViewController(mapViewController, animated: true)
     }
 
     func mapViewController(mapViewController: MapViewController, didSelectMapItem mapItem: MKMapItem) {
         print(mapItem)
-        mapViewController.dismissViewControllerAnimated(true, completion: nil)
+
+        self.navigationController?.popToViewController(self, animated: true)
     }
 
     func resultsViewController(resultsViewController: SearchResultsViewController,
